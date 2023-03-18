b92ed8f99c867692caf052e1c847dd7bb56fdc0c
Showman_Das <Showmandas3216@gmail.com>
2023-03-18 10:19:13 +0600

    total calculation done

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-let getInp=document.getElementById('getInp');
-// console.log(getInp);
-let increase=document.getElementById('increase');
-let decrease=document.getElementById('decrease');
+
 let showPrice=document.getElementById('showPrice');
 let vat=document.getElementById('vat').innerText;
 let total=document.getElementById('total');
-increase.addEventListener('click',()=>{
+
+
+
+function decreaseQuantity(incrDecr){
+    let getInp=document.getElementById(incrDecr);
+    let inpVal=parseInt(getInp.value);
+    if(getInp.value <=0){
+        alert('Invalid quantity');
+    }else{
+        getInp.value=inpVal-1;
+        let getVal=getInp.value;
+        let result=getVal * 10;
+        showPrice.innerText=result;
+        getInp.style.backgroundColor='#fff';
+        getInp.style.Color='#000';
+    }
+
+}
+
+function increaseQuantity(incrDecr){
+    let getInp=document.getElementById(incrDecr);
     let inpVal=parseInt(getInp.value);
     // console.log(inpVal);
-    getInp.value=inpVal+1;
+    if(getInp.value >=10){
+        alert('Maximum 10 quantity is allowed!');
+        getInp.style.backgroundColor='red';
+        getInp.style.Color='#fff';
+    }else{
+        getInp.value=inpVal+1;
     // document.getElementById('getInp').innerHTML=Number(getInp.value)+1;
     let getVal=getInp.value;
     let result=getVal * 10;
@@ -21,19 +43,7 @@ increase.addEventListener('click',()=>{
     // console.log(totalResult);
     total.innerText=totalResult;
 
-})
-
-decrease.addEventListener('click',()=>{
-    let inpVal=parseInt(getInp.value);
-    if(getInp.value <=0){
-        alert('Invalid quantity');
-    }else{
-        getInp.value=inpVal-1;
-        // document.getElementById('getInp').innerHTML=Number(getInp.value)+1;
-        let getVal=getInp.value;
-        let result=getVal * 10;
-        showPrice.innerText=result;
     }
+}
 
-})
 
